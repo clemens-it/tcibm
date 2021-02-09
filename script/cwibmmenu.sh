f6bc17bcebb6ba0c86ad7044a504c0e45f27e702
Clemens Egger <clemens_e@gmx.net>
2021-02-09 16:49:30 +0100

    Change path to default path in cwibmmenu.sh

--- script/cwibmmenu.sh
+++ script/cwibmmenu.sh
@@ -2,9 +2,8 @@
 
 # the purpose of this file is to be called with mintty.exe in an CygWin environment.
 # you can create an shortcut to the following target:
-# C:\wherever-cygwin-is-installed\bin\mintty.exe -he -e /opt/iButtonManager/cwibmmenu.sh
+# C:\wherever-cygwin-is-installed\bin\mintty.exe -he -e /opt/tcibm/cwibmmenu.sh
 
-cd /opt/iButtonManager
+cd /opt/tcibm
 export PATH=/usr/local/bin:/usr/bin
 exec ./ibmmenu.sh
-
